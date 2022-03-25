d5d80c61430f0bedc100f7b46eff46c987bd74f5
Shashikumar <shashirgowdas67@gmail.com>
2022-03-25 17:01:03 +0530

    [Shashi] Add: Inserting employee data values

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,4 +10,12 @@ name varchar(100) not null,
 salary money not null,
 start date not null,
 );
+--check ctable created succesfully
+select * from  employee_payroll
+
+--UC3-Insert Data
+insert into employee_payroll values
+('Shashi',100000.0,'2020-01-03'),
+('Madhu',200000.0,'2021-06-15'),
+('Vinayak',300000.0,'2022-02-21')
 

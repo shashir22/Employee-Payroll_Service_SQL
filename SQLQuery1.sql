9203d0a06c45f679c03503bab3a373b476dddfe8
Shashikumar <shashirgowdas67@gmail.com>
2022-03-25 16:36:05 +0530

    [Shashi] Add:Creating table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,5 +2,12 @@
 create database payroll_service;
 use payroll_service;
 
-
+--UC2-Create Table
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start date not null,
+);
 

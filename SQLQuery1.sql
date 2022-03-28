1604c8ccf3189b98963345c863268bb49f77629e
Shashikumar <shashirgowdas67@gmail.com>
2022-03-28 23:11:48 +0530

    [Shashi] Add::Make Terissa as Part of Sales and Marketing Department

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -60,6 +60,13 @@ taxable_pay money,
 income_tax money,
 net_pay float;
 
+--UC10-Madhu Sales and Marketing Department
+insert into employee_payroll values
+('Madhu',700000.0,'2020-08-14','M','8734586245','sales','india',100.0,12000.0,2000.0,1233.0)
+
+update employee_payroll set taxable_pay=12000.00, department='marketing',income_tax=2000.00, net_pay=1233 where id=2;
+select * from employee_payroll where name='Madhu'
+
 
 
 
